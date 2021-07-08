3ec2c9d729f5a84aae33c59bf2000464127f1b29
克雷斯 <hgkiller@hotmail.com>
2021-07-08 09:22:51 +0800

    第一次提交

---
+++ BlazorApp1/Controller/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public List<string> Get()
+        {
+            return new List<string>() { "a", "b" };
+        }
+        [HttpPost]
+        public PostData Post(PostData test)
+        {
+            
+            return test;
+        }
+        public class PostData
+        {
+            public List<string> Test { get; set; }
+        }
+    }
+}
---
+++ BlazorApp1/Data/TokenProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class TokenProvider
+    {
+        public string AntiforgeryToken { get; set; }
+    }
+}
